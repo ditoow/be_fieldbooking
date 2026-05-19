90b31ebc77e97e1e59305dbad34d72b5d0f2c442
ditoow <ghifariwiraa@gmail.com>
2026-05-19 20:13:59 +0700

    feat: register ScheduleSeeder and BookingSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             FieldSeeder::class,
+            ScheduleSeeder::class,
+            BookingSeeder::class,
         ]);
     }
 }
